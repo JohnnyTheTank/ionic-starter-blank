26874e626156f8d8ff5736323f86f476f3577e05
Jonathan Hornung <jonathan.hornung@gmail.com>
2016-01-30 23:05:02 +0100

    added routing. fixed position of $ionicConfigProvider Stuff

--- js/app.js
+++ js/app.js
@@ -26,11 +26,92 @@ angular.module('starter', [
     if(window.StatusBar) {
       StatusBar.styleDefault();
     }
+  });
+})
+.config(function ($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
 
     //remove view transitions
     //$ionicConfigProvider.views.transition('none');
 
     //no caching
     //$ionicConfigProvider.views.maxCache(0);
-  });
-})
+
+
+
+    // Ionic uses AngularUI Router which uses the concept of states
+    // Learn more here: https://github.com/angular-ui/ui-router
+    // Set up the various states which the app can be in.
+    // Each state's controller can be found in controllers.js
+  /*
+    $stateProvider
+
+    // setup an abstract state for the tabs directive
+        .state('tab', {
+            cache: false,
+            url: '/tab',
+            abstract: true,
+            templateUrl: 'templates/tabs.html'
+        })
+
+        // Each tab has its own nav history stack:
+
+        .state('tab.news', {
+            cache: false,
+            url: '/news',
+            views: {
+                'tab-news': {
+                    templateUrl: 'templates/tab-news.html',
+                    controller: 'NewsCtrl'
+                }
+            }
+        })
+
+        .state('tab.news-detail', {
+            cache: false,
+            url: '/news/:newsId',
+            views: {
+                'tab-news': {
+                    templateUrl: 'templates/news-detail.html',
+                    controller: 'NewsDetailCtrl'
+                }
+            }
+        })
+
+        .state('tab.chats', {
+            cache: false,
+            url: '/chats',
+            views: {
+                'tab-chats': {
+                    templateUrl: 'templates/tab-chats.html',
+                    controller: 'ChatsCtrl'
+                }
+            }
+        })
+        .state('tab.chat-detail', {
+            cache: false,
+            url: '/chats/:chatId',
+            views: {
+                'tab-chats': {
+                    templateUrl: 'templates/chat-detail.html',
+                    controller: 'ChatDetailCtrl'
+                }
+            }
+        })
+
+        .state('tab.account', {
+            cache: false,
+            url: '/account',
+            views: {
+                'tab-account': {
+                    templateUrl: 'templates/tab-account.html',
+                    controller: 'AccountCtrl'
+                }
+            }
+        });
+
+
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/tab/news');
+
+    */
+});
